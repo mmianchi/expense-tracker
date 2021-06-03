bec209dadef8b4eea1949c57bab7dafdc8197551
Mahshid Mianchi <mmianchi@gmail.com>
2021-06-02 23:05:45 -0700

    use one state on form  depends on prev state

--- src/Components/NewExpenses/ExpenseForm.js
+++ src/Components/NewExpenses/ExpenseForm.js
@@ -2,55 +2,50 @@ import React, {useState} from 'react'
 import './ExpenseForm.css'
 const ExpenseFrom = () => {
 
-    // const[enteredTitle, setEnteredTitle] = useState('');
-    // const[enteredAmount, setEnteredAmount] = useState('');
-    // const[enteredDate, setEnteredDate] = useState('');
+    const[enteredTitle, setEnteredTitle] = useState('');
+    const[enteredAmount, setEnteredAmount] = useState('');
+    const[enteredDate, setEnteredDate] = useState('');
 
-
-    const[userInput, setUserInput]= useState({
-                        enteredTitle: '',
-                        enteredAmount: '',
-                        enteredDate:'',
-                    });
     const titleChangeHandler = (event) => {
-        // setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value,
-          
-        })
+        setEnteredTitle(event.target.value);
     }
+
     const amountChangeHandler = (event) => {
-        // setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value,
-          
-        })
+        setEnteredAmount(event.target.value);
     }
+
     const dateChangeHandler = (event) => {
-        // setEnteredDate(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value,
-          
-        })
+        setEnteredDate(event.target.value);
     }
     
+    const submitFromHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData= {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date (enteredDate)
+        }
+        console.log(expenseData)
+        setEnteredTitle('');
+        setEnteredTitle('');
+        setEnteredDate('');
+    }
+
     return (
-        <form>
+        <form onSubmit={submitFromHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input onChange={titleChangeHandler} type="text"/>
+                    <input value={enteredTitle} onChange={titleChangeHandler} type="text"/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input onChange={amountChangeHandler} Amount="number" min="0.01" step="0.01"/>
+                    <input value={enteredAmount} onChange={amountChangeHandler} type="number" min="0.01" step="0.01"/>
                 </div>    
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input onChange={dateChangeHandler} type="date" min="2019-01-01" max="2022-12-31"/>
+                    <input value={enteredDate} onChange={dateChangeHandler} type="date" min="2019-01-01" max="2022-12-31"/>
                 </div>
             </div>
             <div className="new-expense__actions">
